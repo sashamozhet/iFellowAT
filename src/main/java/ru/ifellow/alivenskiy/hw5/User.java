f1cddb64e13638e96a2b1dbdb24d4be458d53d74
Alexandr Livenskiy <trolololoshki@mail.ru>
2025-09-30 15:38:50 +0300

    Добавляю домашку 5.

---
+++ src/main/java/ru/ifellow/alivenskiy/hw5/User.java
@@ -0,0 +1,30 @@
+package ru.ifellow.alivenskiy.hw5;
+
+public class User {
+    private String name;
+    private String job;
+
+    public User(){
+
+    }
+    public User(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
---
+++ src/test/java/ru/ifellow/alivenskiy/hw5/Specifications/Specifications.java
@@ -0,0 +1,32 @@
+package ru.ifellow.alivenskiy.hw5.Specifications;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpecification(String url){
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+    public  static ResponseSpecification responseSpecOK200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+    public  static ResponseSpecification responseSpecUnique(int status){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(status)
+                .build();
+    }
+    public static void installSpecification(RequestSpecification request,
+                                            ResponseSpecification response){
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
+}
